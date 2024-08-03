451380dd20a01e3677ade3b5de3dc5c84d1e4aae
Bsovann <bsovann04@gmail.com>
2024-08-03 13:37:55 -0500

    Rename Service to NailService
    Renamed the Service class to NailService for better clarity and specificity. Updated all references to reflect the new class name. This change aims to avoid any ambiguity in class responsibilities.

--- src/main/java/com/bondith/appointmentservice/model/NailService.java
+++ src/main/java/com/bondith/appointmentservice/model/NailService.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Service {
+public class NailService {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
