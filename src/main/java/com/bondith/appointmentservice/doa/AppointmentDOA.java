22ad72d16b35a1277e0150a109ab2fe7c7744676
Bsovann <bsovann04@gmail.com>
2024-08-03 13:37:32 -0500

    Add query to count conflicting appointments in AppointmentDAO
    Introduced a custom query method `countConflictingAppointments` to check for scheduling conflicts based on technician ID and appointment times. This enhances the capability to efficiently manage appointments and ensure no overlaps for the given technician schedule.

--- src/main/java/com/bondith/appointmentservice/doa/AppointmentDOA.java
+++ src/main/java/com/bondith/appointmentservice/doa/AppointmentDOA.java
@@ -2,10 +2,25 @@ package com.bondith.appointmentservice.doa;
 
 import com.bondith.appointmentservice.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface AppointmentDOA extends JpaRepository<Appointment, Integer> {
     List<Appointment> findByStatus(String status);
+
+    @Query("""
+            SELECT COUNT(a) FROM Appointment a \
+            WHERE a.technician.id = :technicianId \
+            AND TIME(a.appointmentTime) < :requestedEndTime \
+            """)
+    int countConflictingAppointments(@Param("technicianId") int technicianId,
+                                      @Param("requestedStartTime") LocalDateTime requestedStartTime,
+                                      @Param("requestedEndTime") LocalDateTime requestedEndTime);
+
 }
